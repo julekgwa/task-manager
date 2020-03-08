360d56eceb197a3c5d17f7631c6327e1a8980ec6
Junius Lekgwara <phutigravel@gmail.com>
2020-03-09 00:18:59 +0200

    first commit

---
+++ src/styles/colors.js
@@ -0,0 +1,12 @@
+export const Colors = {
+  veryDarkGrayishOrange: '#353432',
+  darkModerateOrange: '#B0935E',
+  darkModerateOrange2: '#977F53',
+  slightlyDesaturatedOrange: '#D1B98B',
+  verySoftOrange: '#E3C994',
+  veryDarkDesaturatedLimeGreen: '#66A65C',
+  slightlyDesaturatedLimeGreen: '#73C867',
+  veryDarkBlue: '#030304', // mostly black
+  veryDarkCyanLimeGreen: '#01552E',
+  pureCyanLimeGreen: '#00DC5E', // (or mostly pure) 
+}
